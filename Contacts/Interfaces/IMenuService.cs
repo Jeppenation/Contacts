b3b7438703f111b78b2655fe0e54ee975b36310b
jesper kallioniemi <hwilajesper@live.com>
2023-12-15 14:24:36 +0100

    Started with DisplayMenu in main program

---
+++ Contacts/Interfaces/IMenuService.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Contacts.Interfaces
+{
+    public interface IMenuService
+    {
+        /// <summary>
+        /// Displays the menu
+        /// </summary>
+        /// <returns>void</returns> 
+
+        void DisplayMenu();
+
+
+        /// <summary>
+        ///Takes the user input and converts into a INT 
+        /// </summary>
+        /// <returns>Returns choice as an int</returns>
+        public int GetMenuChoice();
+    }
+}
---
+++ Contacts/Services/MenuServices.cs
@@ -0,0 +1,61 @@
+﻿using Contacts.Interfaces;
+using Contacts.Shared.Interfaces;
+using Contacts.Shared.Models;  
+using Contacts.Shared.Services;
+
+namespace Contacts.Services
+{
+    public class MenuServices : IMenuService
+    {
+        public int GetMenuChoice()
+        {
+            Console.Write("\nPlease enter your choice: ");
+            var choice = Console.ReadLine();
+            return Convert.ToInt32(choice);
+
+        }
+
+        public void DisplayMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("*** Welcome to the Contact App ***"); //<- - This is the menu
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Show All Contacts");
+                Console.WriteLine("3: Show Specific Contact");
+                Console.WriteLine("4. Remove Contact\n");
+                Console.WriteLine("55. Exit");
+
+                var choice = GetMenuChoice();
+
+                switch(choice)
+                {
+                    case 1:
+                        AddContactMenu();
+                        break;
+                    case 2:
+                        Console.WriteLine("Show All Contacts");
+                        break;
+                    case 3:
+                        Console.WriteLine("Show Specific Contact");
+                        break;
+                    case 4:
+                        Console.WriteLine("Remove Contact");
+                        break;
+                    case 55:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+
+            }
+        }
+
+        private void AddContactMenu()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
