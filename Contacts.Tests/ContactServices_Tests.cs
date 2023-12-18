0b60b54e55a756b5970adae00644608a7627aa10
jesper kallioniemi <hwilajesper@live.com>
2023-12-18 01:09:26 +0100

    Created a simple test

---
+++ Contacts.Tests/ContactServices_Tests.cs
@@ -0,0 +1,30 @@
+﻿using Contacts.Shared.Interfaces;
+using Contacts.Shared.Models;
+using Contacts.Shared.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Tests
+{
+    public class ContactServices_Tests
+    {
+        [Fact]
+        public void AddToList_ShouldAddContactToList_ThenReturnTrue()
+        {
+            // Arrange
+            IContact contact = new Contact { FirstName = "John", LastName = "Doe", EmailAddress = "test@domain.com", Address = "teststreet 123", City = "Test City", PhoneNumber = "555-12345" };
+            IFileService fileService = new FileService();
+            IContactService contactService = new ContactServices(fileService);
+
+            // Act
+            var result = contactService.AddContact(contact);
+
+            // Assert
+            Assert.True(result);
+
+        }
+    }
+}
